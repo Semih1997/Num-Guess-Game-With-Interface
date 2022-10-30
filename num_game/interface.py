e47e39c88bc67c6d9cef7d50e968aa205685d195
Semih1997 <emiroglusemih97@gmail.com>
2022-10-30 21:45:11 +0300

    More Generic Geometry
    Each geomerty and button height and width write 1 line for each. If i decide to change them i don't have to look all buttons and screen geometry.

--- num_game/interface.py
+++ num_game/interface.py
@@ -1,41 +1,44 @@
-from multiprocessing.resource_sharer import stop
-import tkinter
-from tkinter import Place, Toplevel, messagebox
+from tkinter import *
 from pc_functions import *
 
+main_screen_geomerty = "400x500"
+in_game_screen_geomerty = "300x400"
+button_height = 5
+button_width = 50
+
 def with_pc_game():                         #Game with Pc Screen
-    pc_game_screen = tkinter.Tk
+    pc_game_screen = Tk
     pc_game_screen = Toplevel(number_game)
     pc_game_screen.title("Come Play")
-    pc_game_screen.geometry("300x400")
+    pc_game_screen.geometry(in_game_screen_geomerty)
     pc_game_screen.mainloop()
 def multiplayer_game():                     #Game with Friend Screen
-    multiplayer_game_screen = tkinter.Tk
+    multiplayer_game_screen = Tk
     multiplayer_game_screen = Toplevel(number_game)
-    multiplayer_game_screen.title("Have Fun With Your Friend")
+    multiplayer_game_screen.title(in_game_screen_geomerty)
     multiplayer_game_screen.geometry("300x400")
     multiplayer_game_screen.mainloop()
 def quiting():
     number_game.quit()
     
-number_game = tkinter.Tk()
+number_game = Tk()
 
 number_game.title("Number Game")
-number_game.geometry("400x500")
+number_game.geometry(main_screen_geomerty)
 
-pc_button = tkinter.Button(number_game,
+pc_button = Button(number_game,
                            text = "Play with PC",
-                           height = 5,
-                           width= 50,
+                           height = button_height,
+                           width= button_width,
                            command = with_pc_game)
 pc_button.pack(pady = 15)
-multip_button =tkinter.Button(number_game,
-                              height = 5,
-                              width = 50,
+multip_button = Button(number_game,
+                              height = button_height,
+                              width = button_width,
                               text = "Play Multiplayer",
                               command = multiplayer_game)
 multip_button.pack(pady = 50)
-quit_button =tkinter.Button(number_game,
+quit_button = Button(number_game,
                               height = 3,
                               width = 25,                              
                               text = "QUIT",
